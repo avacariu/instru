8238116dda01ca550bb0380e47f2fa05e3818401
Andrei Vacariu <andrei@avacariu.me>
2017-10-24 13:00:24 -0700

    Add a detach function

--- src/instrumenter/instru.c
+++ src/instrumenter/instru.c
@@ -10,15 +10,26 @@ instru_eval_frame(PyFrameObject *frame, int throwflag)
 }
 
 PyObject *
-instru_attach_evaler(PyObject *self, PyObject *args)
+instru_attach(PyObject *self, PyObject *args)
 {
     PyThreadState *tstate = PyThreadState_GET();
     tstate->interp->eval_frame = instru_eval_frame;
+
+    Py_RETURN_NONE;
+}
+
+PyObject *
+instru_detach(PyObject *self, PyObject *args)
+{
+    PyThreadState *tstate = PyThreadState_GET();
+    tstate->interp->eval_frame = _PyEval_EvalFrameDefault;
+
     Py_RETURN_NONE;
 }
 
 static PyMethodDef instru_methods[] = {
-    {"attach", instru_attach_evaler, METH_VARARGS, "Attach the new frame evaluation function."},
+    {"attach", instru_attach, METH_VARARGS, "Attach the new frame evaluation function."},
+    {"detach", instru_detach, METH_VARARGS, "Detach"},
     {NULL, NULL, 0, NULL}
 };
 
