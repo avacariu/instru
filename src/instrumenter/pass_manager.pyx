68139949d178f33211e7283caffb7433b1152f54
Andrei Vacariu <andrei@avacariu.me>
2017-11-08 18:19:04 -0800

    Fix issue with infinite recursion

--- src/instrumenter/pass_manager.pyx
+++ src/instrumenter/pass_manager.pyx
@@ -5,11 +5,24 @@ from .frame_eval cimport (PyThreadState_Get, PyObject, PyFrameObject,
 from .cfg cimport CFG
 
 
+cdef bint should_instrument(filename):
+    for disallowed in ['/dis.py', '/cfg.pyx']:
+        if filename.endswith(disallowed):
+            return False
+
+    # TODO: figure out why this happens
+    if filename == '<string>':
+        return False
+
+    return True
+
+
 cdef PyObject *eval_frame(PyFrameObject *frame_obj, int throwflag):
     frame = <object> frame_obj
     cdef str filename = frame.f_code.co_filename
 
-    frame_cfg = CFG(<object> frame_obj.f_code)
+    if should_instrument(filename):
+        frame_cfg = CFG(<object> frame_obj.f_code)
 
     return _PyEval_EvalFrameDefault(frame_obj, throwflag)
 
