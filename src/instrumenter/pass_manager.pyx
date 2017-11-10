4f5818fd179bcc4f3ffdf912004ba03d6963cdf3
Andrei Vacariu <andrei@avacariu.me>
2017-11-10 10:51:59 -0800

    Create new code object with new consts and names
    The new constants and names will be used for logging the path through
    the CFG, and this is just the first step toward creating new bytecode
    that will take advantage of these new names.
    
    The proper list of constants will be added when the CFG is actually
    analysed.

--- src/instrumenter/pass_manager.pyx
+++ src/instrumenter/pass_manager.pyx
@@ -1,9 +1,12 @@
 from libc.stdio cimport printf
 
-from .frame_eval cimport (PyThreadState_Get, PyObject, PyFrameObject,
-                          PyThreadState, _PyEval_EvalFrameDefault)
+from .frame_eval cimport (PyThreadState_Get, PyObject, PyCodeObject,
+                          PyFrameObject, PyThreadState,
+                          _PyEval_EvalFrameDefault)
 from .cfg cimport CFG
 from .logger cimport log
+
+from types import CodeType
 
 
 cdef set _instrumented_hashes = set()
@@ -21,12 +24,42 @@ cdef bint should_instrument(object frame):
 
 
 cdef PyObject *eval_frame(PyFrameObject *frame_obj, int throwflag):
+    cdef int num_consts, num_names
+
     frame = <object> frame_obj
 
     if should_instrument(frame):
         print("Instrumenting fn:", frame.f_code.co_name)
         print("In file:", frame.f_code.co_filename)
         log(hash(frame.f_code), 1, 0.2, _log_fd)
+
+        new_consts = tuple(list(frame.f_code.co_consts) + [5])
+        new_names = tuple(list(frame.f_code.co_names) + ['__instru_counter'])
+
+        new_code = CodeType(
+            frame.f_code.co_argcount,
+            frame.f_code.co_kwonlyargcount,
+            frame.f_code.co_nlocals,
+            frame.f_code.co_stacksize,
+            frame.f_code.co_flags,
+            frame.f_code.co_code,
+            new_consts,
+            new_names,
+            frame.f_code.co_varnames,
+            frame.f_code.co_filename,
+            frame.f_code.co_name,
+            frame.f_code.co_firstlineno,
+            frame.f_code.co_lnotab,
+            frame.f_code.co_freevars,
+            frame.f_code.co_cellvars
+        )
+
+        frame_obj.f_code = <PyCodeObject *> new_code
+
+        print("#names", len(frame.f_code.co_names))
+        print("names", frame.f_code.co_names)
+        print("#consts", len(frame.f_code.co_consts))
+        print("consts", frame.f_code.co_consts)
 
         frame_cfg = CFG(<object> frame_obj.f_code)
 
